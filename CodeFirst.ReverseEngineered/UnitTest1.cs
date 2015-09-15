9fe69a86f17c0cc38e2b0d842e91f1fe5d738c76
Manuel Riezebosch <mriezebosch@gmail.com>
2015-09-15 11:58:26 +0200

    Reverse Engineered met VS2015

---
+++ CodeFirst.ReverseEngineered/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeFirst.ReverseEngineered
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
